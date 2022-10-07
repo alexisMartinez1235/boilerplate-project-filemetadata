154c0aa53028642f0724a98eb4be1474a2455cf4
Alexis Martinez <67340366+alexisMartinez1235@users.noreply.github.com>
2022-10-07 23:24:18 +0000

    i add apifileanalyse route with upload single file option and response with name, type and size of file requested

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
 require('dotenv').config()
 
-var app = express();
+const app = express();
+const multer  = require('multer');
+const upload = multer({
+  dest: './public/data/uploads/'
+});
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,8 +15,15 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const { file } = req;
+  
+  res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size,
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
